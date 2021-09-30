c173ce1083128e6e31e84ec5b3ef1cf035b6da14
fares <faresboukhezna@gmail.com>
2021-09-30 15:09:36 +0200

    basic frontend with api consumption

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 function App() {
+
+  const [apiData, setApiData] = useState(null);
+
+  const GetDatas = async () => {
+    let data = await axios.get("nodeapp:8080/books");
+    console.log(data);
+    setApiData(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-          Learn React
+          This is react
       </header>
+
+      <button onClick={() => GetDatas()}>
+        Pull data from api
+      </button>
+
+      {apiData && <p>{apiData}</p>}
     </div>
   );
 }
